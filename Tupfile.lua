fc023b6e3363e3e6253726603684063ba646544c
Rendaw <rendaw@zarbosoft.com>
2014-10-23 18:12:53 +0900

    Switched to ren-tupsupport tupfiles.

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,18 +1,12 @@
-local objects = {}
-
-for index, source in ipairs(tup.glob('*.c'))
-do
-	local out = source .. '.o'
-	tup.definerule{
-		inputs = {source},
-		outputs = {out},
-		command = 'gcc -ansi -pedantic -Wall -Werror -ggdb -O0 -fPIC -c -o ' .. out .. ' ' .. source
-	}
-	objects[#objects + 1] = out
-end
-
-tup.definerule{
-	inputs = objects,
-	outputs = {'luxem.so'},
-	command = 'gcc -ansi -pedantic -Wall -Werror -ggdb -O0 -fPIC -shared -o luxem.so ' .. table.concat(objects, ' ')
+LuxemCObjects = Define.Objects
+{
+	Sources = Item 'luxem_internal_common.c' + 'luxem_rawread.c' + 'luxem_rawwrite.c',
+	IsLibrary = true,
 }
+
+local LuxemC = Define.Library
+{
+	Name = 'luxem',
+	Objects = LuxemCObjects,
+}
+
